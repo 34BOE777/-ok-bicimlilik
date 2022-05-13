ab25ace9ac60bee6d8c2e4b6180c38ee4d4d6c49
34BOE777 <98213674+34BOE777@users.noreply.github.com>
2022-05-13 13:26:38 +0300

    Add files via upload

---
+++ cu.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+
+class dik{
+private: 
+	int alan;
+public:
+	dik(int alan):alan(alan){}
+	int getalan(){
+		return alan;
+	}
+};
+
+class boya{
+public:
+	boya(){}
+	int maliyet(int a){
+		return a*70;
+	}
+};
+
+class duvar:public dik,public boya{
+public:
+	duvar(int x):dik(x){}
+	
+	void yazdir(){
+	cout<<"Toplam alan: "<<getalan()<<"\nBoyama maliyeti: "<<maliyet(getalan())<<endl;
+	}
+};
+
+int main(int argc, char const *argv[])
+{
+	duvar d;
+	duvar d1(35);
+	d1.yazdir();
+	return 0;
+}
+
+
+
+
+
+
